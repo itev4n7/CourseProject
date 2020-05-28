e5772a5128011176446afc5643cba78dc9171565
Aleksey <loha.malyota@gmail.com>
2020-05-28 14:44:45 +0300

    create project

---
+++ src/test/java/com/it/driver/DriverFactory.java
@@ -0,0 +1,33 @@
+package com.it.driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static com.it.common.Constants.BASE_URL;
+
+public class DriverFactory {
+
+    public static WebDriver getDriver() {
+        WebDriver driver = null;
+        String driverName = System.getProperty("driver");
+        if (driverName != null) {
+            if ("fireFox".equals(driverName)) {
+                System.setProperty("webdriver.gecko.driver", "drivers/geckodriver");
+                driver = new FirefoxDriver();
+            } else if ("chrome".equals(driverName)) {
+                System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+                driver = new ChromeDriver();
+            }
+        } else {
+            System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+            driver = new ChromeDriver();
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get(BASE_URL);
+        return driver;
+    }
+}
